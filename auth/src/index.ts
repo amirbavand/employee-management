181cd9caf5c9a98a1bd51d4eec4db5b570ababd1
amirbavand <amirhossein.bavand@gmail.com>
2021-03-06 09:15:47 -0500

    connected to mongo

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,3 +6,31 @@ import mongoose from "mongoose";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+
+app.all("*", async (req, res) => {
+  throw new Error();
+});
+
+const start = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb://mongo-authentication-service:27017/auth",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    );
+    console.log("Connected to MongoDb");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
