cfac76a34e56d659963289c02a27ef6faeaebb69
amirbavand <amirhossein.bavand@gmail.com>
2021-03-06 11:55:39 -0500

    some changes in auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,19 +1,26 @@
 import express from "express";
-import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
-
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
+import cookieSession from "cookie-session";
+
 const app = express();
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 
 app.all("*", async (req, res) => {
+  console.log("no roote error");
   throw new Error();
 });
 
@@ -31,6 +38,9 @@ const start = async () => {
   } catch (err) {
     console.error(err);
   }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!!!!");
+  });
 };
 
 start();
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,55 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { validateRequest } from "../middle-ware/validate-request";
+
+import { Password } from "../workers/password";
+import { User } from "../models/user";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Hello world!");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+
+  async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+
+    const existingUser = await User.findOne({ username });
+    if (!existingUser) {
+      console.log("there is no user");
+      throw new Error("Invalid credentials");
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordsMatch) {
+      throw new Error("Invalid Credentials");
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        username: existingUser.username,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
