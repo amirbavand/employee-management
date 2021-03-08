1b7f3dbe16859f07ee8f073bbb0c68f39a658cf9
amirbavand <amirhossein.bavand@gmail.com>
2021-03-07 19:46:23 -0500

    tests for edit completed

--- employee/src/routes/__test__/edit.test.ts
+++ employee/src/routes/__test__/edit.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
 import { signinMock } from "../../test/signin-mock";
+import { Employee } from "../../models/employee";
 
 it("should return error by inserting ", async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -162,4 +163,84 @@ it("returning error 40 by invalid request", async () => {
       Title: "",
     })
     .expect(400);
+});
+
+it("should return error if try to assign a employee id that exists in the database", async () => {
+  const cookie = signinMock();
+
+  const response1 = await request(app)
+    .post("/api/employees")
+    .set("Cookie", cookie)
+    .send({
+      EmployeeId: "amir",
+      Name: "amir",
+      Surname: "bavand",
+      PhoneNumber: "123456",
+      Address: "Canada",
+      Title: "software engineer",
+    });
+
+  const response2 = await request(app)
+    .post("/api/employees")
+    .set("Cookie", cookie)
+    .send({
+      EmployeeId: "james",
+      Name: "james",
+      Surname: "james",
+      PhoneNumber: "123456",
+      Address: "Canada",
+      Title: "software engineer",
+    });
+
+  await request(app)
+    .put(`/api/employees/${response1.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      EmployeeId: "james",
+      Name: "amir",
+      Surname: "bavand",
+      PhoneNumber: "123456",
+      Address: "Canada",
+      Title: "Computer Scientist",
+    })
+    .expect(400);
+});
+
+it("edit the emolyee with valid input", async () => {
+  const cookie = signinMock();
+
+  const response = await request(app)
+    .post("/api/employees")
+    .set("Cookie", cookie)
+    .send({
+      EmployeeId: "amir",
+      Name: "amir",
+      Surname: "bavand",
+      PhoneNumber: "123456",
+      Address: "Canada",
+      Title: "software engineer",
+    });
+
+  await request(app)
+    .put(`/api/employees/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      EmployeeId: "amir",
+      Name: "amir",
+      Surname: "bavand",
+      PhoneNumber: "123456",
+      Address: "Canada",
+      Title: "Computer Scientist",
+    })
+    .expect(200);
+
+  const employee = await Employee.find({});
+
+  expect(employee[0].EmployeeId).toEqual("amir");
+  expect(employee[0].Name).toEqual("amir");
+  expect(employee[0].Surname).toEqual("bavand");
+  expect(employee[0].PhoneNumber).toEqual("123456");
+  expect(employee[0].Address).toEqual("Canada");
+  expect(employee[0].Title).toEqual("Computer Scientist");
+  expect(employee[0].id).toEqual(response.body.id);
 });
