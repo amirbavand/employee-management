3bafb76af7f14a0c1cdc0b1ed24351bd84ddc32b
amirbavand <amirhossein.bavand@gmail.com>
2021-03-08 19:02:19 -0500

    some changes in test file of login

--- client/src/compnenets/__test__/login.test.js
+++ client/src/compnenets/__test__/login.test.js
@@ -3,8 +3,18 @@ import ReactDom from "react-dom";
 import Login from "../login/login";
 import { mount } from "enzyme";
 
+let component;
+
+beforeEach(() => {
+  component = mount(<Login />);
+});
+
+afterEach(() => {
+  component.unmount();
+});
+
 it("has a submit button", () => {
-  const component = mount(<Login />);
   expect(component.find("form").length).toEqual(1);
   expect(component.find("button").length).toEqual(1);
+  expect(component.find("input").length).toEqual(2);
 });
