9e27030d6fa029a81c2ed731462b0681d5f92aa4
amirbavand <amirhossein.bavand@gmail.com>
2021-03-10 08:40:00 -0500

    table show list initiated

--- client/src/compnenets/show-table/show-table.js
+++ client/src/compnenets/show-table/show-table.js
@@ -1,18 +1,105 @@
 import React, { Component } from "react";
 import queryString from "query-string";
 import axios from "axios";
+import "./show-table.css";
 
 class ShowTable extends Component {
+  state = {
+    loaded: false,
+  };
   async componentDidMount() {
     const apiPath = "api/employees" + this.props.quryParams;
     try {
       const values = await axios.get(apiPath);
-      console.log(values.data);
+      console.log(values.data[1].EmployeeId);
       console.log("this is data");
     } catch (error) {}
   }
   render() {
-    return <div>this is show component page</div>;
+    if (this.state.loaded === false) return <h2>table is loading</h2>;
+    return (
+      <div>
+        <table>
+          <tr>
+            <th>EmployeeId</th>
+            <th>Name</th>
+            <th>Surename</th>
+            <th>PhoneNumber</th>
+            <th>Address</th>
+            <th>Title</th>
+            <th>
+              <button>Deail</button>
+            </th>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Alfreds Futterkiste</td>
+            <th>
+              <button>Deail</button>
+            </th>
+          </tr>
+          <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Centro comercial Moctezuma</td>
+            <th>
+              <button>Deail</button>
+            </th>
+          </tr>
+          <tr>
+            <td>Ernst Handel</td>
+            <td>Roland Mendel</td>
+            <td>Austria</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Centro comercial Moctezuma</td>
+            <th>
+              <button>Deail</button>
+            </th>
+          </tr>
+          <tr>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Centro comercial Moctezuma</td>
+            <th>
+              <button>Deail</button>
+            </th>
+          </tr>
+          <tr>
+            <td>Laughing Bacchus Winecellars</td>
+            <td>Yoshi Tannamuri</td>
+            <td>Canada</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Centro comercial Moctezuma</td>
+            <th>
+              <button>Deail</button>
+            </th>
+          </tr>
+          <tr>
+            <td>Magazzini Alimentari Riuniti</td>
+            <td>Giovanni Rovelli</td>
+            <td>Italy</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Centro comercial Moctezuma</td>
+            <th>
+              <button>Deail</button>
+            </th>
+          </tr>
+        </table>
+      </div>
+    );
   }
 }
 
